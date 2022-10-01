03a4caa4401547ea5d2885c3dcc78075ae3e3601
Gustavo <54654696+gusta7pires@users.noreply.github.com>
2022-09-30 21:20:47 -0300

    bullet script

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,4 +1,18 @@
-extends Node2D
+extends Area2D
+
+var speed = 500
+var velocity : Vector2
+
+func _ready():
+	velocity.x = 1
+	velocity.y = 0
+
+func _process(delta):
+	position += velocity * speed * delta
+
+func direction(dir):
+	velocity.x = dir.x
+	velocity.y = dir.y 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- Scripts/PlayerShoot.gd
+++ Scripts/PlayerShoot.gd
@@ -1,27 +1,41 @@
 extends KinematicBody2D
 
-onready var bullet = preload("res://Scenes/Bullet.tscn")
+export (PackedScene) var bullet
 
 var bullet_position
-var bullet_speed = 100
+var bullet_velocity : Vector2 = Vector2.ZERO
+var can_shoot = true
 
 func _process(delta):
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right") :
 		$AnimatedSprite.play("run_shoot_up")
 		$BulletPosition.position = Vector2(8, -14)
-		bullet_position = 1
+		#bullet_position = 1
+		bullet_velocity.x = 1
+		bullet_velocity.y = -1
 	elif Input.is_action_pressed("ui_up"):
 		$AnimatedSprite.play("idle_look_up")
 		$BulletPosition.position = Vector2(0, -20)
-		bullet_position = 2
+		#bullet_position = 2
+		bullet_velocity.x = 0
+		bullet_velocity.y = -1
 	else:
 		$AnimatedSprite.play("idle");
 		$BulletPosition.position = Vector2(12, -1)
-		bullet_position = 0
+		#bullet_position = 0
+		bullet_velocity.x = 1
+		bullet_velocity.y = 0
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		var bullet_instance = bullet.instance()
-		get_parent().add_child(bullet_instance)
-		bullet_instance.global_position = $BulletPosition.global_position
+	if Input.is_action_pressed("ui_accept"):
+		if can_shoot:
+			var bullet_instance = bullet.instance()
+			get_parent().add_child(bullet_instance)
+			bullet_instance.global_position = $BulletPosition.global_position
+			bullet_instance.direction(bullet_velocity)
+			can_shoot = false
+			$CoolDownTimer.start()
 		
-		
+	
+
+func _on_CoolDownTimer_timeout():
+	can_shoot = true
