193a09cfb5431bc98412791158d057d9fb6e3d93
Gustavo <gpires94@gmail.com>
2022-10-05 00:25:12 -0300

    bullet detection

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
 var speed = 500
+export(Array, Texture) var bullet_type
+var sprite
 var velocity : Vector2
+var hit_player = false
 
 func _ready():
+	sprite = $Sprite
 	velocity.x = 1
 	velocity.y = 0
 
@@ -13,9 +17,18 @@ func _process(delta):
 func set_speed(speed):
 	self.speed = speed
 
-func set_direction(dir):
+func set_bullet(speed, type, dir, hitplayer):
+	self.speed = speed
+	sprite.texture = bullet_type[type]
 	velocity.x = dir.x
-	velocity.y = dir.y 
+	velocity.y = dir.y
+	self.hit_player = hit_player  
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_Bullet_body_entered(body):
+	if "Enemy" in body.name:
+		body.queue_free()
+		queue_free()
--- Scripts/PlayerShoot.gd
+++ Scripts/PlayerShoot.gd
@@ -1,123 +1,161 @@
 extends KinematicBody2D
 
 export (PackedScene) var bullet
+var bullet_speed_player = 500
 
 var facing_right = true
+var is_jumping = false
 var is_shooting = false
 
-var bullet_position
 var bullet_direction : Vector2 = Vector2.ZERO
 var can_shoot = true
 
+var animated_sprite_node
+var bullet_position_node
+var cool_down_timer_node
+
+func _ready():
+	animated_sprite_node = $AnimatedSprite
+	bullet_position_node = $BulletPosition
+	cool_down_timer_node = $CoolDownTimer
+
 func _process(delta):
+	jump()
+	move()
+	shoot()
+
+func move():
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left"):
 		if facing_right:
 			flip_player()
 		shooting_directions("up_left")
-		$AnimatedSprite.play("run_shoot_up")
+		if not is_jumping:
+			animated_sprite_node.play("run_shoot_up")
 		
 	elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right"):
 		if not facing_right:
 			flip_player()
 		shooting_directions("up_right")
-		$AnimatedSprite.play("run_shoot_up")
+		if not is_jumping:
+			animated_sprite_node.play("run_shoot_up")
 		
 	elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left"):
 		if facing_right:
 			flip_player()
 		shooting_directions("down_left")
-		$AnimatedSprite.play("run_shoot_down")
+		if not is_jumping:
+			animated_sprite_node.play("run_shoot_down")
 		
 	elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right"):
 		if not facing_right:
 			flip_player()
 		shooting_directions("down_right")
-		$AnimatedSprite.play("run_shoot_down")
+		if not is_jumping:
+			animated_sprite_node.play("run_shoot_down")
 		
 	elif Input.is_action_pressed("ui_up"):
 		shooting_directions("up")
-		$AnimatedSprite.play("idle_look_up")
+		if not is_jumping:
+			animated_sprite_node.play("idle_look_up")
 		
 	elif Input.is_action_pressed("ui_down"):
-		if facing_right:
-			shooting_directions("lowered_right")
+		if is_jumping:
+			shooting_directions("down")
 		else:
-			shooting_directions("lowered_left")
-		$AnimatedSprite.play("idle_lowered")
+			if facing_right:
+				shooting_directions("lowered_right")
+			else:
+				shooting_directions("lowered_left")
+			animated_sprite_node.play("idle_lowered")
 		
 	elif Input.is_action_pressed("ui_left"):
 		if facing_right:
 			flip_player()
 		shooting_directions("left")
-		if is_shooting:
-			$AnimatedSprite.play("run_shoot")
-		else:
-			$AnimatedSprite.play("run")
+		if not is_jumping:
+			if is_shooting:
+				animated_sprite_node.play("run_shoot")
+			else:
+				animated_sprite_node.play("run")
 		
 	elif Input.is_action_pressed("ui_right"):
 		if not facing_right:
 			flip_player()
 		shooting_directions("right")
-		if is_shooting:
-			$AnimatedSprite.play("run_shoot")
-		else:
-			$AnimatedSprite.play("run")
+		if not is_jumping:
+			if is_shooting:
+				animated_sprite_node.play("run_shoot")
+			else:
+				animated_sprite_node.play("run")
 		
 	else:
 		if facing_right:
 			shooting_directions("right")
 		else:
 			shooting_directions("left")
-		$AnimatedSprite.play("idle")
-	
-	if Input.is_action_pressed("ui_accept"):
+		if not is_jumping:
+			animated_sprite_node.play("idle")
+
+func jump():
+	if Input.is_action_just_pressed("jump"):
+		is_jumping = !is_jumping
+	if is_jumping:
+		animated_sprite_node.play("jump")
+
+func shoot():
+	if Input.is_action_pressed("shoot"):
 		is_shooting = true
 		if can_shoot:
 			var bullet_instance = bullet.instance()
 			get_parent().add_child(bullet_instance)
-			bullet_instance.global_position = $BulletPosition.global_position
-			bullet_instance.set_direction(bullet_direction)
+			bullet_instance.global_position = bullet_position_node.global_position
+			bullet_instance.set_scale(Vector2(3,3))
+			bullet_instance.set_bullet(bullet_speed_player, 0, bullet_direction, false)
 			can_shoot = false
-			$CoolDownTimer.start()
+			cool_down_timer_node.start()
 	else:
 		is_shooting = false
 
 func shooting_directions(direction):
 	match direction:
 		"up":
-			$BulletPosition.position = Vector2(0, -20)
+			bullet_position_node.position = Vector2(0, -20)
 			bullet_direction.x = 0
 			bullet_direction.y = -1
 		"up_left":
-			$BulletPosition.position = Vector2(-8, -14)
+			bullet_position_node.position = Vector2(-8, -14)
 			bullet_direction.x = -1
 			bullet_direction.y = -1
 		"up_right":
-			$BulletPosition.position = Vector2(8, -14)
+			bullet_position_node.position = Vector2(8, -14)
 			bullet_direction.x = 1
 			bullet_direction.y = -1
 		"left":
-			$BulletPosition.position = Vector2(-12, -1)
+			bullet_position_node.position = Vector2(-12, -1)
 			bullet_direction.x = -1
 			bullet_direction.y = 0
 		"right":
-			$BulletPosition.position = Vector2(12, -1)
+			bullet_position_node.position = Vector2(12, -1)
 			bullet_direction.x = 1
 			bullet_direction.y = 0
+		"down":
+			bullet_position_node.position = Vector2(0, 20)
+			bullet_direction.x = 0
+			bullet_direction.y = 1
 		"down_left":
-			$BulletPosition.position = Vector2(-10, 7)
+			bullet_position_node.position = Vector2(-10, 7)
 			bullet_direction.x = -1
 			bullet_direction.y = 1
 		"down_right":
-			$BulletPosition.position = Vector2(10, 7)
+			bullet_position_node.position = Vector2(10, 7)
 			bullet_direction.x = 1
 			bullet_direction.y = 1
 		"lowered_left":
-			$BulletPosition.position = Vector2(-17, 13)
+			bullet_position_node.position = Vector2(-17, 13)
 			bullet_direction.x = -1
 			bullet_direction.y = 0
 		"lowered_right":
-			$BulletPosition.position = Vector2(17, 13)
+			bullet_position_node.position = Vector2(17, 13)
 			bullet_direction.x = 1
 			bullet_direction.y = 0
 	
@@ -125,10 +163,10 @@ func shooting_directions(direction):
 func flip_player():
 	if(facing_right):
 		facing_right = false
-		$AnimatedSprite.flip_h = true
+		animated_sprite_node.flip_h = true
 	else:
 		facing_right = true
-		$AnimatedSprite.flip_h = false
+		animated_sprite_node.flip_h = false
 
 func _on_CoolDownTimer_timeout():
 	can_shoot = true
