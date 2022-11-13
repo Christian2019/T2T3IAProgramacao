915d457f6576e407ee1c87671d10aaac23357eb8
Mateus Gautério <50017946+MateusABG@users.noreply.github.com>
2022-11-12 21:36:48 -0300

    Add files via upload

---
+++ Scripts/Mateus/EndGame.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+var speed=5
+var pisca=false
+
+var tempo=0
+var senoTempo=0
+var visibility=true
+
+func _ready(): 
+	$MusicaFundo.play()
+	$Timer.start()
+	
+func flashText():
+	if !pisca:
+		if(senoTempo>0):
+			visibility=true
+		else:
+			visibility=false
+	else:
+		visibility=true
+		modulate.a=senoTempo
+
+func _physics_process(delta):
+	tempo+=delta
+	senoTempo=sin(tempo*speed)  
+	$FinalScreen/Mensagem.visible=visibility  
+	flashText()
+
+
+func _on_Timer_timeout() -> void:
+	get_tree().change_scene("res://Scenes/RootScenes/StartScreen.tscn")
