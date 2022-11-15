1b06bf064a03bd389e9278be20a80b82781c82f5
Mateus Gautério <50017946+MateusABG@users.noreply.github.com>
2022-11-15 18:47:20 -0300

    Add files via upload

--- Scripts/PlayerAnimation.gd
+++ Scripts/PlayerAnimation.gd
@@ -14,49 +14,63 @@ var animation2=false
 var animation3=false
 var dancemfer=false
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready(): 
+		turn_animation1()
+		timerCreator("turn_animation2",30,[],true)     
+		timerCreator("turn_animation3",50,[],true)     
+		timerCreator("turn_animation4",70,[],true)     
 
-func MOONWALK(): 
-	animation1=false   
+func turn_animation1():
+	animation1=true
+
+func turn_animation2():
+	animation2=true
+	animation1=false
+
+func turn_animation3():
+	animation3=true
+	animation2=false
+	animation1=false
+
+func turn_animation4():
+	animation3=false
+	animation2=false
+	animation1=false
+			 
+func MOONWALK():    
 	if(animation2):
-		play("WALK") 
+		self.animation="WALK"  
 		if(position.x<=finalx2):
 			position.x+=3
 		else:
 			position.x-=3
 
-func ROLL():  
-	play("ROLL")
-	if( position.x <=finalx):
-		position.x+=2
-	elif(position.x >=finalx):
-		position.x-=2
+func ROLL():      
+	if(animation1):
+		self.animation="ROLL"  
+		if( position.x <=finalx):
+			position.x+=2
+		elif(position.x >=finalx):
+			position.x-=2
 
-func WALK():  
-	animation2=false  
+func WALK():    
 	if(animation3):
-		play("WALK") 
+		self.animation="WALK"  
 		if(position.x < finalx3):
 			position.x+=2
 		elif(position.x >finalx3):
 			position.x-=2
  
-
-func play_dance():
-	animation3=false;
-	animation2=false 
-	if(not animation1 and not animation2 and not animation3): 
-		play("DANCE")
+func play_dance(): 
+	if(not animation1 and not animation2 and not animation3):
+		self.animation="DANCE"
 	
 func _process(delta):
-		if(animation1):
-			ROLL()  
-		animation2=true
-		timerCreator("MOONWALK",30,[],true)    
-		animation3=true
-		timerCreator("WALK",50,[],true)     
-		timerCreator("play_dance",70,[],true)     
+	ROLL()
+	MOONWALK()
+	WALK()
+	play_dance()
+	 
 	 
 
 func timerCreator(functionName,time,parameters,create):
